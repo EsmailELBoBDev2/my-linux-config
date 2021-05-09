b15a7a4d7719d237a5db809f148b653404fcf48b
Esmail EL BoB <github.defilable@simplelogin.co>
2021-05-09 12:48:20 +0200

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,23 @@
+#bin/usr/bash
+
+echo "WELCOME. <https://www.youtube.com/watch?v=3KAEInRUXEc>"
+
+# ============================ # 
+
+## Install APPs:
+sudo pacman -S doas mpv picard calibre exodus librewolf-bin ungoogled-chromium alsa-control cozy-audiobooks devour droidcam-obs-plugin-git freetube-bin github-desktop-bin lanxchange sublime-text-3 turtl vscodium-bin bleachbin celluloid handbreak kleopatra obs-studio qbittorrent gparted rednotebook virt-manager brave dnscrypt-proxy neofetch os-prober  
+
+## Edit their configs:
+cd Downloads || mkdir Downloads 
+
+# doas
+wget https://raw.githubusercontent.com/EsmailELBoBDev2/my-linux-config/main/Configs/doas.conf
+sudo mv doas.conf /etc/doas.conf
+
+# dnscrypt-proxy
+wget https://raw.githubusercontent.com/EsmailELBoBDev2/my-linux-config/main/Configs/dnscrypt-proxy.toml
+sudo mv dnscrypt-proxy.toml /etc/dnscrypt-proxy/dnscrypt-proxy.toml
+
+# ============================ # 
+echo "THANK YOU. <https://www.youtube.com/watch?v=7aKN1AC6MQU>"
+return 0
